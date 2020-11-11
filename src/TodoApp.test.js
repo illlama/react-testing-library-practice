2c23ce11bac7d28965dc2a05bf44d81dcef071d2
illlama <beatlemaniakih@gmail.com>
2020-11-11 18:51:33 +0900

    toggle 구현

--- src/TodoApp.test.js
+++ src/TodoApp.test.js
@@ -18,6 +18,7 @@ describe('<TodoApp />', () => {
   //새로운 항목을 추가
   it('creates new todo', () => {
     const { getbyPlaceholderText, getByText } = render(<TodoApp />);
+    //이벤트를 발생시켜서 새로운 항목을 추가하면
     fireEvent.change(getbyPlaceholderText('할 일을 입력하세요'), {
       target: {
         value: '새 항목 추가하기',
@@ -25,5 +26,16 @@ describe('<TodoApp />', () => {
     });
     fireEvent.click(getByText('Submit'));
     getByText('새 항목 추가하기');
+  });
+  //Toggle
+  it('toggles todo', () => {
+    const { getByText } = render('TDD 배우기');
+    //TDD 배우기 항목에 클릭을 발생 후 text-decoration의 속성 확인
+    const todoText = getByText('TDD 배우기');
+    expect(todoText).toHaveStyle('text-decoration: line-through;');
+    fireEvent.click(todoText);
+    expect(todoText).not.toHaveStyle('text-decoration: line-through;');
+    fireEvent.click(todoText);
+    expect(todoText).toHaveStyle('text-decoration: line-through;');
   });
 });
